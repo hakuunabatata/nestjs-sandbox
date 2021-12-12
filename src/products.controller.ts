7907fbd12c052ee88a0c91f6fb1a696f7817cf81
hakuunabatata <emachadomartins1999@gmail.com>
2021-12-12 14:19:40 -0300

    create productscontroller

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get()
   getAll(): string {
     return 'Produtos';
+  }
+
+  @Get(':id')
+  getOne(@Param() { id }): string {
+    return `Product ${id}`;
+  }
+
+  @Post()
+  create(@Body() product): string {
+    console.log(product);
+    return 'Created!';
+  }
+
+  @Put()
+  update(@Body() product): string {
+    console.log(product);
+    return 'Updated!';
+  }
+
+  @Delete(':id')
+  delete(@Param() { id }): string {
+    return `Delete ${id}!`;
   }
 }
