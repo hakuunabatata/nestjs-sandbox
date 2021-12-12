b335beb616ca58fe2a744bea20fdccfb105d405c
hakuunabatata <emachadomartins1999@gmail.com>
2021-12-12 15:14:27 -0300

    create productsmodel

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -7,33 +7,38 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Product } from './products.model';
 
 @Controller('products')
 export class ProductsController {
+  products: Product[] = [
+    new Product('LIV01', 'Duna', 35),
+    new Product('LIV02', 'Fundacao', 20.5),
+    new Product('LIV03', 'O Senhor dos aneis', 25),
+  ];
+
   @Get()
-  getAll(): string {
-    return 'Produtos';
+  getAll(): Product[] {
+    return this.products;
   }
 
   @Get(':id')
-  getOne(@Param() { id }): string {
-    return `Product ${id}`;
+  getOne(@Param() { id }): Product {
+    return { ...this.products[0], id };
   }
 
   @Post()
-  create(@Body() product): string {
-    console.log(product);
-    return 'Created!';
+  create(@Body() product: Product) {
+    this.products.push(product);
   }
 
   @Put()
-  update(@Body() product): string {
-    console.log(product);
-    return 'Updated!';
+  update(@Body() product: Product): Product {
+    return product;
   }
 
   @Delete(':id')
-  delete(@Param() { id }): string {
-    return `Delete ${id}!`;
+  delete(@Param() { id }) {
+    this.products.pop();
   }
 }
